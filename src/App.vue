39d30561d479143f3960ff06d24e8d40aa49c8e4
Ana Iris Padilla Maradiaga <apadilla@bancatlan.hn>
2026-01-30 23:55:31 -0600

    feat: search zip code using external API

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,6 @@ defineOptions({
 
 <template>
   <div>
-    <div class="w-full h-dvh flex flex-col justify-center items-center">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-      <h1>Frontend challenge</h1>
-    </div>
-
     <nav>
       <router-link to="/">Search</router-link> |
       <router-link to="/saved">Saved</router-link>
--- src/views/ZipSearchView.vue
+++ src/views/ZipSearchView.vue
@@ -1,5 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { getZipInfo } from '../api/zippopotam'
+
+const zip = ref('')
+const loading = ref(false)
+const error = ref('')
+const data = ref<any>(null)
+
+async function searchZip() {
+  error.value = ''
+  data.value = null
+
+  if (zip.value.length !== 5) {
+    error.value = 'Enter a 5-digit ZIP'
+    return
+  }
+
+  try {
+    loading.value = true
+    data.value = await getZipInfo(zip.value)
+  } catch (e) {
+    error.value = 'ZIP not found'
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
 <template>
   <div>
-    Zip Search View 2
+    <h2>ZIP Search</h2>
+
+    <input v-model="zip" placeholder="Enter ZIP" />
+    <button @click="searchZip">Search</button>
+
+    <p v-if="loading">Loading...</p>
+    <p v-if="error">{{ error }}</p>
+
+    <div v-if="data">
+      <p><b>City:</b> {{ data.places[0]['place name'] }}</p>
+      <p><b>State:</b> {{ data.places[0].state }}</p>
+      <p><b>Country:</b> {{ data.country }}</p>
+      <p><b>Latitude:</b> {{ data.places[0].latitude }}</p>
+      <p><b>Longitude:</b> {{ data.places[0].longitude }}</p>
+    </div>
   </div>
 </template>
